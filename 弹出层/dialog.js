3d1aaf1e8ab7dea2156643e43e7fe0f59a364714
Administrator <1786452818@qq.com>
2018-05-26 10:55:01 +0800

    前端

--- 弹出层/dialog.js
+++ 弹出层/dialog.js
@@ -11,23 +11,5 @@ var dialog={
 
 
     }
-        var html= '<div class="dislog-mask">'
-            +'<div class="dislog-box">'
-            +'<div class="dialog-title">'
-            +'<h2>友情提示</h2>'
-            +'<span class="dialog-close">X</span>'
-            +'</div>'
-            +'<div class="dialog-content">哈哈</div>'
-            +'</div>'
-            +'</div>';
-        $('body').append(html);
-        $('.dialog-box').css({
-        width:obj.width,
-        height:obj.height
-    });
-$( '.dialog-title h2').html(obj,title);
-$('.dialog-content').load(obj,content);
-$('.dialog-close').load(obj,function(){
-       $('.dialog-mask').remove();
-    })
+
 }
---
+++ 模块--轮播图/carousel.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2018/5/26.
+ */
+define([''],function(){
+    function Carousel(){
+        this.container=$('<div id="container"></div>>');
+        this.prev=$('<span id="prev">&lt;</span>');
+        this.next=$('<span id="next">&gt;</span>');
+        this.tab= $('<ul id="tab"></ul>');
+
+    }
+})
