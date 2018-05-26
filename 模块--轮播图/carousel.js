7549d4f0da1902d098cca462745c19b7dd0f26c1
Administrator <1786452818@qq.com>
2018-05-26 11:13:21 +0800

    前端

--- 模块--轮播图/carousel.js
+++ 模块--轮播图/carousel.js
@@ -7,6 +7,15 @@ define([''],function(){
         this.prev=$('<span id="prev">&lt;</span>');
         this.next=$('<span id="next">&gt;</span>');
         this.tab= $('<ul id="tab"></ul>');
-
     }
-})
+    Carousel.prototype.init= function(){
+        var option={
+            selector:'#box',
+            imgData:['img/1.jpg','img/2.jpg','img/3.jpg','img/4.jpg']
+        };
+        for(var i=0;i<option.imgData.length ;i++){
+            $(this.tab).append('<li class="${i==0?"selected";""}">${i+1}</li>')
+            $(this.container).append('')
+        }
+    }
+});
